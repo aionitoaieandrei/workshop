528d81e38345ebe3e5a6976cadd6793176bc5c96
madalina.laios <madaliina.laios@gmail.com>
2019-09-23 15:16:05 +0300

    Added elements

--- 3pgSeleniumWebDriverExamples/src/test/java/Main.java
+++ 3pgSeleniumWebDriverExamples/src/test/java/Main.java
@@ -1,21 +1,28 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
     public static void main(String[] args) {
-        //  System.setProperty("webdriver.chrome.driver", "C:\\Users\\MadalinaPc\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "3pgSeleniumWebDriverExamples/chromedriver.exe");
         String pageUrl = "http://automationpractice.com/index.php";
         // WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.get(pageUrl);
-        WebElement element = driver.findElement(By.id(""));
-        WebElement element1 = driver.findElement(By.className(""));
-        WebElement element2 = driver.findElement(By.tagName(""));
-        WebElement element3 = driver.findElement(By.partialLinkText(""));
-        WebElement element4 = driver.findElement(By.linkText(""));
-        WebElement element5 = driver.findElement(By.name(""));
-        driver.close();
+        WebElement searchInput = driver.findElement(By.id("search_query_top"));
+        searchInput.sendKeys("dress");
+        //first method
+        searchInput.sendKeys(Keys.ENTER);
+        //second method
+        WebElement searchButton = driver.findElement(By.name("submit_search"));
+        searchButton.click();
+//        WebElement element1 = driver.findElement(By.className(""));
+//        WebElement element2 = driver.findElement(By.tagName(""));
+//        WebElement element3 = driver.findElement(By.partialLinkText(""));
+//        WebElement element4 = driver.findElement(By.linkText(""));
+//        WebElement element5 = driver.findElement(By.name(""));
+        driver.quit();
     }
 }
